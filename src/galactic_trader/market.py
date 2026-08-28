ff3b26c9192ce1c1df762b85667742c8f3922e1a
Spark2000 <paulalex.wetzel@gmail.com>
2026-08-28 19:30:24 +0200

    added validation for undesired values in Market, including tests

--- src/galactic_trader/market.py
+++ src/galactic_trader/market.py
@@ -5,16 +5,31 @@ from galactic_trader.products import ProductType
 
 @dataclass
 class Market:
-    """For each product type, there is one market"""
+    """
+    Represents a market for one product.
+
+    product : associated ProductType
+    current_price : current price to buy 1 of the product
+    volatility : sets the price fluctations on buying/ selling of the product
+    """
 
     product: ProductType
     current_price: float
     volatility: float
 
+    def __post_init__(self) -> None:
+        """Validates the initial market values."""
+        if self.volatility < 0:
+            raise ValueError("Volatility must be positive.")
+        if self.current_price < 1:
+            raise ValueError("Current price must be greater or equal to one.")
+
     def adjust_price(self, direction: int) -> None:
         """
         Adjusts price based on supply/demand.
-        direction: 1 for Buy (Demand up), -1 for Sell (Supply up).
+        direction:
+            1 for Buy (demand up -> price up),
+            -1 for Sell (supply up -> price down).
         """
         change = direction * self.volatility
         new_price = self.current_price + change
--- src/galactic_trader/tests/test_market.py
+++ src/galactic_trader/tests/test_market.py
@@ -1,3 +1,5 @@
+import pytest
+
 from galactic_trader.market import *
 
 
@@ -8,9 +10,20 @@ def test_adjust_price() -> None:
 
     assert market.current_price == 10.5
 
+
 def test_adjust_price_below_one() -> None:
     market = Market(product=ProductType.FOOD, current_price=10.0, volatility=9.5)
 
     market.adjust_price(-1)
 
     assert market.current_price == 1.0
+
+
+def test_initial_volatility_value_too_low() -> None:
+    with pytest.raises(ValueError):
+        Market(product=ProductType.FOOD, current_price=10.0, volatility=-1)
+
+
+def test_initial_current_price_value_too_low() -> None:
+    with pytest.raises(ValueError):
+        Market(product=ProductType.FOOD, current_price=0.5, volatility=0.5)
